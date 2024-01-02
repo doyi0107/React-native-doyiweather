c8ed3c2e84572fc43dc950cc62a2fb750610e64a
doyi <doyi9917@naver.com>
2024-01-02 13:43:33 +0900

    react-native start

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>안녕! React-native wep에 처음 접근했어!</Text>
       <StatusBar style="auto" />
     </View>
   );

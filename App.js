d9858780a112fa6311b2391b3be470d9a78ef1d8
doyi <doyi9917@naver.com>
2024-01-04 20:20:18 +0900

    styleSheet -ing

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
+
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>안녕! React-native wep에 처음 접근했어!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.city}>
+        <Text style={styles.cityname}>Incheon</Text>
+      </View>
+      <View style={styles.weather}>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+      </View>
     </View>
   );
 }
@@ -13,12 +20,34 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "orange",
   },
-  text:{
-    fontSize:28,
-    color:"skyblue",
+  city:{
+    flex:1,
+    justifyContent:"center",
+    alignItems:"center"
+  },
+  cityname:{
+       fontSize:68,
+       fontWeight:500
+  },
+  weather:{
+    flex:3,
+  },
+  day:{
+    flex:1,
+    backgroundColor:"teal",
+    alignItems:"center"
+  },
+  temp:{
+    marginTop:50,
+    fontSize:178
+  },
+  description:{
+    marginTop:-10,
+    fontSize:50
   }
 });
+
+
+// npx expo login

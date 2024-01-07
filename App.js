4e4f960eba9c3ba54ab0649dfb3d36b692707d0b
doyi <doyi9917@naver.com>
2024-01-07 12:46:24 +0900

    add weather api

--- App.js
+++ App.js
@@ -1,15 +1,48 @@
+import * as Location from "expo-location";
+import { useEffect,useState } from "react";
+import { StyleSheet, Dimensions, Text, View, ScrollView } from "react-native";
 
-import { StyleSheet, Dimensions, Text, View ,ScrollView} from 'react-native';
-
-const { width:SCREEN_WIDTH } = Dimensions.get("window");
+const { width: SCREEN_WIDTH } = Dimensions.get("window");
 
 export default function App() {
+  const [city, Setcity] = useState("Loading...");
+  const [days, SetDays] = useState([]);
+  const [ok, setOk] = useState(true);
+
+  const API_KEY = "3ec3b59f14f08e45d16702c5f6e927b1";
+
+  const getWeather = async() => {
+      const {granted} = await Location.requestForegroundPermissionsAsync();
+      if(!granted){
+        setOk(false)
+    }
+
+    const {coords:{latitude,longitude}} = await Location.getCurrentPositionAsync({ accuracy: 5 });
+    const location = await Location.reverseGeocodeAsync({latitude,longitude} ,{useGoogleMaps:false});
+    Setcity(location[0].city);
+
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`
+    );
+    const json = await response.json();
+    console.log(json)
+  };
+
+  useEffect(() => { 
+
+    getWeather();
+  }, []);
   return (
     <View style={styles.container}>
       <View style={styles.city}>
-        <Text style={styles.cityname}>Incheon</Text>
+        <Text style={styles.cityname}>{city}</Text>
       </View>
-      <ScrollView pagingEnabled showsHorizontalScrollIndicator={false} horizontal contentContainerStyle={styles.weather}>
+      <ScrollView
+        pagingEnabled
+        showsHorizontalScrollIndicator={false}
+        horizontal
+        contentContainerStyle={styles.weather}
+      >
         <View style={styles.day}>
           <Text style={styles.temp}>27</Text>
           <Text style={styles.description}>Sunny</Text>
@@ -36,30 +69,28 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "orange",
   },
-  city:{
-    flex:1,
-    justifyContent:"center",
-    alignItems:"center"
+  city: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
   },
-  cityname:{
-       fontSize:68,
-       fontWeight:500
+  cityname: {
+    fontSize: 68,
+    fontWeight: 500,
   },
-  weather:{
+  weather: {},
+  day: {
+    width: 390,
+    alignItems: "center",
   },
-  day:{
-    width:390,
-    alignItems:"center"
+  temp: {
+    marginTop: 50,
+    fontSize: 178,
   },
-  temp:{
-    marginTop:50,
-    fontSize:178
+  description: {
+    marginTop: -10,
+    fontSize: 50,
   },
-  description:{
-    marginTop:-10,
-    fontSize:50
-  }
 });
-
 
 // npx expo login

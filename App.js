b03856d6ea5ba90de2c6a11df8665b17c970eae2
doyi <doyi9917@naver.com>
2024-01-08 23:43:06 +0900

    weather error

--- App.js
+++ App.js
@@ -1,13 +1,56 @@
 import * as Location from "expo-location";
 import { useEffect,useState } from "react";
-import { StyleSheet, Dimensions, Text, View, ScrollView } from "react-native";
+import {
+  StyleSheet,
+  Dimensions,
+  Text,
+  View,
+  ScrollView,
+  ActivityIndicator,
+} from "react-native";
 
 const { width: SCREEN_WIDTH } = Dimensions.get("window");
 
 export default function App() {
-  const [city, Setcity] = useState("Loading...");
-  const [days, SetDays] = useState([]);
+  const [city, setcity] = useState("Loading...");
+  const [days, setDays] = useState([]);
   const [ok, setOk] = useState(true);
+  setTimeout(() => {
+    setDays([
+      {
+        temp: { day: 19.49 },
+        weather: [{ main: "Clouds", description: "broken clouds" }],
+      },
+      {
+        temp: { day: 22.03 },
+        weather: [{ main: "Clear", description: "clear sky" }],
+      },
+      {
+        temp: { day: 19.92 },
+        weather: [{ main: "Clouds", description: "few clouds" }],
+      },
+      {
+        temp: { day: 19.39 },
+        weather: [{ main: "Clouds", description: "overcast clouds" }],
+      },
+      {
+        temp: { day: 19.7 },
+        weather: [{ main: "Clouds", description: "few clouds" }],
+      },
+      {
+        temp: { day: 17.19 },
+        weather: [{ main: "Clouds", description: "broken clouds" }],
+      },
+      {
+        temp: { day: 17.32 },
+        weather: [{ main: "Rain", description: "light rain" }],
+      },
+      {
+        temp: { day: 18.04 },
+        weather: [{ main: "Clouds", description: "few clouds" }],
+      },
+    ]);
+  }, 2000);
 
   const API_KEY = "3ec3b59f14f08e45d16702c5f6e927b1";
 
@@ -19,19 +62,26 @@ export default function App() {
 
     const {coords:{latitude,longitude}} = await Location.getCurrentPositionAsync({ accuracy: 5 });
     const location = await Location.reverseGeocodeAsync({latitude,longitude} ,{useGoogleMaps:false});
-    Setcity(location[0].city);
+    setcity(location[0].city);
 
-    const response = await fetch(
-      `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`
-    );
-    const json = await response.json();
-    console.log(json)
-  };
 
-  useEffect(() => { 
+      const { list } = await (
+        await fetch(
+          `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}`
+        )
+      ).json();
+      const filteredList = list.filter(({ dt_txt }) => dt_txt.endsWith("00:00:00"));
+      setDays(filteredList);
 
-    getWeather();
-  }, []);
+
+
+      };
+
+      useEffect(() => { 
+
+        getWeather();
+        
+      }, []);
   return (
     <View style={styles.container}>
       <View style={styles.city}>
@@ -43,22 +93,18 @@ export default function App() {
         horizontal
         contentContainerStyle={styles.weather}
       >
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Sunny</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Sunny</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Sunny</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Sunny</Text>
-        </View>
+        {days.length === 0 ? (
+          <View style={styles.day}>
+            <ActivityIndicator color="white" style={{marginTop:10}} size="large"/>
+          </View>
+        ) : (
+          days.map((day,index)=>{
+                <View key={index} style={styles.day}>
+                      <Text style={styles.temp}>{day.temp}</Text>
+                      <Text style={styles.description}>{day.weather[0].main}</Text>
+                </View>
+          })
+        )}
       </ScrollView>
     </View>
   );

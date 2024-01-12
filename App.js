57c8178035680bfd1c0fa8543d059e05023a974b
doyi <doyi9917@naver.com>
2024-01-12 19:28:08 +0900

    weather+icon add

--- App.js
+++ App.js
@@ -1,142 +1,148 @@
 import * as Location from "expo-location";
-import { useEffect,useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
-  StyleSheet,
-  Dimensions,
-  Text,
   View,
-  ScrollView,
+  Text,
+  Dimensions,
   ActivityIndicator,
+  StyleSheet,
+  ScrollView,
 } from "react-native";
+import { Fontisto } from "@expo/vector-icons";
 
 const { width: SCREEN_WIDTH } = Dimensions.get("window");
 
+
+const icons = {
+  Clouds: "cloudy",
+  Clear: "day-sunny",
+  Atmosphere: "cloudy-gusts",
+  Snow: "snow",
+  Rain: "rains",
+  Drizzle: "rain",
+  Thunderstorm: "lightning",
+};
+
+//개인 api_key오류로 노마드코더님것을 잠시 빌렸다,,,ㅠㅠ
+const API_KEY = "784ab24ff2ed5d94d4288abed9e25d13";
+
 export default function App() {
-  const [city, setcity] = useState("Loading...");
+  const [city, setCity] = useState("Loading...");
   const [days, setDays] = useState([]);
   const [ok, setOk] = useState(true);
-  setTimeout(() => {
-    setDays([
-      {
-        temp: { day: 19.49 },
-        weather: [{ main: "Clouds", description: "broken clouds" }],
-      },
-      {
-        temp: { day: 22.03 },
-        weather: [{ main: "Clear", description: "clear sky" }],
-      },
-      {
-        temp: { day: 19.92 },
-        weather: [{ main: "Clouds", description: "few clouds" }],
-      },
-      {
-        temp: { day: 19.39 },
-        weather: [{ main: "Clouds", description: "overcast clouds" }],
-      },
-      {
-        temp: { day: 19.7 },
-        weather: [{ main: "Clouds", description: "few clouds" }],
-      },
-      {
-        temp: { day: 17.19 },
-        weather: [{ main: "Clouds", description: "broken clouds" }],
-      },
-      {
-        temp: { day: 17.32 },
-        weather: [{ main: "Rain", description: "light rain" }],
-      },
-      {
-        temp: { day: 18.04 },
-        weather: [{ main: "Clouds", description: "few clouds" }],
-      },
-    ]);
-  }, 2000);
 
-  const API_KEY = "3ec3b59f14f08e45d16702c5f6e927b1";
-
-  const getWeather = async() => {
-      const {granted} = await Location.requestForegroundPermissionsAsync();
-      if(!granted){
-        setOk(false)
+  const getWeather = async () => {
+    const { granted } = await Location.requestForegroundPermissionsAsync();
+    if (!granted) {
+      setOk(false);
     }
+    const {
+      coords: { latitude, longitude },
+    } = await Location.getCurrentPositionAsync({ accuracy: 5 });
 
-    const {coords:{latitude,longitude}} = await Location.getCurrentPositionAsync({ accuracy: 5 });
-    const location = await Location.reverseGeocodeAsync({latitude,longitude} ,{useGoogleMaps:false});
-    setcity(location[0].city);
+    const location = await Location.reverseGeocodeAsync(
+      { latitude, longitude },
+      { useGoogleMaps: false }
+    );
 
+    setCity(location[0].city);
 
-      const { list } = await (
-        await fetch(
-          `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}`
-        )
-      ).json();
-      const filteredList = list.filter(({ dt_txt }) => dt_txt.endsWith("00:00:00"));
-      setDays(filteredList);
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=alerts&appid=${API_KEY}&units=metric`
+    );
 
+    const json = await response.json();
 
+    console.log(json.daily)
 
-      };
-
-      useEffect(() => { 
-
-        getWeather();
-        
-      }, []);
+    setDays(json.daily);
+  };
+  useEffect(() => {
+    getWeather();
+  }, []);
   return (
     <View style={styles.container}>
       <View style={styles.city}>
-        <Text style={styles.cityname}>{city}</Text>
+        <Text style={styles.cityName}>{city}</Text>
       </View>
       <ScrollView
         pagingEnabled
-        showsHorizontalScrollIndicator={false}
         horizontal
+        showsHorizontalScrollIndicator={false}
         contentContainerStyle={styles.weather}
       >
-        {days.length === 0 ? (
-          <View style={styles.day}>
-            <ActivityIndicator color="white" style={{marginTop:10}} size="large"/>
+        { days.length === 0 ? (
+          <View style={{...styles.day, alignItems:"center"}}>
+            <ActivityIndicator
+              color="white"
+              style={{ marginTop: 10 }}
+              size="large"
+            />
           </View>
         ) : (
-          days.map((day,index)=>{
-                <View key={index} style={styles.day}>
-                      <Text style={styles.temp}>{day.temp}</Text>
-                      <Text style={styles.description}>{day.weather[0].main}</Text>
-                </View>
-          })
+          days.map((day, index) => (
+            <View key={index} style={styles.day}>
+              <View
+                style={{
+                  flexDirection: "row",
+                  alignItems: "flex-end",
+                  width: "100%",
+                }}
+              >
+                <Text style={styles.temp}>
+                  {parseFloat(day.temp.day).toFixed(1)}
+                </Text>
+                <Fontisto
+                  name={icons[day.weather[0].main]}
+                  size={100}
+                  color="white"
+                />
+              </View>
+              <Text style={styles.description}>{day.weather[0].main}</Text>
+              <Text style={styles.tinyText}>{day.weather[0].description}</Text>
+            </View>
+          ))
         )}
       </ScrollView>
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "orange",
   },
   city: {
-    flex: 1,
+    flex: 1.2,
     justifyContent: "center",
     alignItems: "center",
   },
-  cityname: {
-    fontSize: 68,
-    fontWeight: 500,
+  cityName: {
+    fontSize: 58,
+    fontWeight: "500",
+    color: "white",
   },
   weather: {},
   day: {
-    width: 390,
-    alignItems: "center",
+    width: SCREEN_WIDTH,
+    // marginLeft: 40,
+    // alignItems: "center",
   },
   temp: {
     marginTop: 50,
+    fontWeight: "600",
     fontSize: 178,
+    color: "white",
   },
   description: {
-    marginTop: -10,
-    fontSize: 50,
+    marginTop: -30,
+    fontSize: 60,
+    marginLeft: 30,
+    color: "white",
+  },
+  tinyText: {
+    fontSize: 20,
+    marginLeft: 30,
+    color: "white",
   },
 });
-
-// npx expo login
